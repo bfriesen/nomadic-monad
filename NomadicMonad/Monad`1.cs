171d2f19469c43b62adcffc3681cc0b97b49215d
Brian Friesen <brian.friesen@gmail.com>
2012-06-30 23:41:21 -0400

    Added Value property to Monad<T>.

--- NomadicMonad/Monad`1.cs
+++ NomadicMonad/Monad`1.cs
@@ -2,6 +2,16 @@
 {
     public class Monad<T>
     {
+        private readonly T _value;
 
+        public Monad(T value)
+        {
+            _value = value;
+        }
+
+        public T Value
+        {
+            get { return _value; }
+        }
     }
 }
