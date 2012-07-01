03f474cb173b5f793b6070b9891391e908961eef
Brian Friesen <brian.friesen@gmail.com>
2012-07-01 00:25:26 -0400

    Added ToMonad extension method.

---
+++ NomadicMonad/Monad.cs
@@ -0,0 +1,10 @@
+﻿namespace RandomSkunk.NomadicMonad
+{
+    public static class Monad
+    {
+        public static Monad<T> ToMonad<T>(this T value)
+        {
+            return new Monad<T>(value);
+        }
+    }
+}
