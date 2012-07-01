9b623f5dba161df774e18875a6758a798320bd3d
Brian Friesen <brian.friesen@gmail.com>
2012-06-30 23:50:26 -0400

    Added demo project with simple constructor usage.

---
+++ NomadicMonad.Demo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace RandomSkunk.NomadicMonad.Demo
+{
+    class Program
+    {
+        static void Main()
+        {
+            PublicConstructorSimpleUsage();
+
+            Console.Write("Press any key to continue...");
+            Console.ReadKey(true);
+        }
+
+        private static void PublicConstructorSimpleUsage()
+        {
+            var m = new Monad<int>(128);
+            Console.WriteLine("The value of m is {0}.", m.Value);
+        }
+    }
+}
